1e2f533266e24d9388c5344135e3bca837e4b61f
chriswycoff <wycoffc@air-weigh.com>
2020-11-28 10:46:43 -0800

    trying to pipe

--- index.php
+++ index.php
@@ -2,4 +2,30 @@
 
 echo "HELLO WORLD";
 
+$username = "chris";
+$password = "P@ssword";
+$database = "mydb";
+$mysqli = new mysqli("localhost", $username, $password, $database);
+
+$query = "SELECT * FROM Candidate";
+echo "<b> <center>Database Output</center> </b> <br> <br>";
+
+if ($result = $mysqli->query($query)) {
+
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["Person_ssn"];
+        $field2name = $row["Party_party_code"];
+        $field3name = $row["Position_pos_id"];
+
+        echo '<b>'.$field1name.$field2name.'</b><br />';
+        echo $field3name.'<br />';
+    }
+
+/*freeresultset*/
+$result->free();
+}
+
+
+
+
 ?>
