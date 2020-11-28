1c33ddbb948ece6c558834d1e58aafb674f7dbc4
chriswycoff <wycoffc@air-weigh.com>
2020-11-28 11:44:49 -0800

    raw html table

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>';
+echo '<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>';
+echo '<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css"></script>';
 
 echo "HELLO WORLD";
 
@@ -11,10 +14,12 @@ $query = "SELECT * FROM Candidate";
 echo "<b> <center>Database Output</center> </b> <br> <br>";
 
 if ($result = $mysqli->query($query)) {
+    echo '<table id="myTable" class="display">';
 echo     '<thead>
 <tr>
     <th>'.'Person_ssn'.'</th>
     <th>'.'Party_party_code'.'</th>
+    <th>'.'Position_pos_id'.'</th>
 </tr>
 </thead>';
 
@@ -22,23 +27,25 @@ echo     '<thead>
         $field1name = $row["Person_ssn"];
         $field2name = $row["Party_party_code"];
         $field3name = $row["Position_pos_id"];
-
-       echo '<table id="table_id" class="display">
-    <tbody>
+        
+    echo ' <tbody>
         <tr>
             <td>'.$field1name.'</td>
             <td>'.$field2name.'</td>
             <td>'.$field3name.'</td>
-        </tr>
-    </tbody>
-    </table>';
+        </tr>';
     }
+    echo '</tbody>
+    </table>';
 
 /*freeresultset*/
 $result->free();
 }
 
-
+// echo "<script>$(document).ready( function () {
+//     $('#myTable').DataTable();
+// } );</script>";
+// for some reason buggy? ^^^^ 
 
 
 ?>
