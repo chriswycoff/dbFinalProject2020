43ea3260277cf907d8c1c536ecd49c0a1cc7fd7d
chriswycoff <wycoffc@air-weigh.com>
2020-12-02 20:21:37 -0800

    production branch with globals switched

--- globals.php
+++ globals.php
@@ -1,6 +1,7 @@
 <?php
 
-$development = true;
+$development = false;
+// add rules here and below to make it work on ~epeterson...
 
 if ($development == false){
     $directory_path = "/~cwycoff/databases_class/dbFinalProject2020/";
