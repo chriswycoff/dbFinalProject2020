6ab650660736f78dd17dc4ae37def6cf5c3e223d
chriswycoff <wycoffc@air-weigh.com>
2020-12-05 10:15:42 -0800

    should work on zeke's server

--- globals.php
+++ globals.php
@@ -1,10 +1,17 @@
 <?php
 
 $development = false;
+$zeke = true;
 // add rules here and below to make it work on ~epeterson...
 
-if ($development == false){
-    $directory_path = "/~cwycoff/databases_class/dbFinalProject2020/";
+if ($development){
+
+    if ($zeke) {
+        "/~ezekielp/databases_class/dbFinalProject2020/";
+    }
+    else{
+        $directory_path = "/~cwycoff/databases_class/dbFinalProject2020/";
+    }
 }
 else{
     $directory_path = "";
